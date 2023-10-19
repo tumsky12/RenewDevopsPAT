0d5d4ac7aa274af7788f0318b0d5d4457a8a247b
Arkius <arkiustum@gmail.com>
2023-10-19 23:32:34 +0100

    first commit

---
+++ Infrastructure/Terraform/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
+  }
+
+  backend "azurerm" {}
+
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+provider "azuread" {}
